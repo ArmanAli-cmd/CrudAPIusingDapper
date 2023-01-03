3f531212399c11a2845b270b6157d907f5ea9577
Arman Ali <aakhana00@gmail.com>
2023-01-03 11:49:21 +0530

    Update Course.cs

--- WebApplicationUsingDapper/Domain/CourseAggregate/Course.cs
+++ WebApplicationUsingDapper/Domain/CourseAggregate/Course.cs
@@ -1,10 +1,12 @@
-﻿namespace WebApplicationUsingDapper.Domain.CourseAggregate
+using WebApplicationUsingDapper.Domain.StudentAggregate;
+
+namespace WebApplicationUsingDapper.Domain.CourseAggregate
 {
     public class Course
     {
         public int CourseId { get; set; } 
         public string CourseName { get; set;}
         public int Price { get; set; }
-
+        public List<String> Students { get; set; }
     }
 }
